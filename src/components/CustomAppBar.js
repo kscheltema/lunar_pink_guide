03c3f7bc8eeb9a041a685ef4cb69e7277cb25751
kscheltema <k87scheltema@outlook.com>
2022-01-22 17:46:37 +0200

    AppBar & Home Page
    Material UI used for AppBar

---
+++ src/components/CustomAppBar.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+import { AppBar, Grid, Toolbar } from "@material-ui/core";
+import styles from "../ui/theme";
+
+function Home() {
+  //   const classes = styles();
+  return (
+    <Fragment>
+      <Grid container md={12} justify="center" direction="row">
+        <AppBar position="static" variant="contained">
+          <Toolbar
+          //   className={classes.appBar}
+          >
+            <p>blah</p>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+    </Fragment>
+  );
+}
+
+export default Home;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import CustomAppBar from "../components/CustomAppBar";
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="The-Pink-Guide-header">
+        <CustomAppBar />
+        <p>blah</p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
